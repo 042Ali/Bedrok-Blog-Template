9d4c63bb5f86cc4f70dbabe3111a212119dceb91
042Ali <ali20090609@gmail.com>
2025-01-27 16:42:56 +0400

    All Blog Posts was half ready to leave

--- src/components/AllPostsPage/AllPostsPage.jsx
+++ src/components/AllPostsPage/AllPostsPage.jsx
@@ -1,7 +1,61 @@
+import "./AllPostsPage.css";
+
 export default function AllPostsPage() {
   return (
     <>
-      <h1>Hello</h1>
+      <div className="All__Blog--Posts">
+        <h1>All Blog Posts</h1>
+        <div className="Grid__Posts">
+          <div className="All__Blog__Posts">
+            <img src="./Frame--387.png" alt="" />
+            <p>NEUTRAL</p>
+            <h1>
+              7 Things about <br /> web design your <br /> boss wants you to
+              <br /> know
+            </h1>
+          </div>
+          <div className="All__Blog__Posts">
+            <img src="./Frame--387.png" alt="" />
+            <p>NEUTRAL</p>
+            <h1>
+              7 Things about <br /> web design your <br /> boss wants you to
+              <br /> know
+            </h1>
+          </div>
+          <div className="All__Blog__Posts">
+            <img src="./Frame--387.png" alt="" />
+            <p>NEUTRAL</p>
+            <h1>
+              7 Things about <br /> web design your <br /> boss wants you to
+              <br /> know
+            </h1>
+          </div>
+          <div className="All__Blog__Posts">
+            <img src="./Frame--387.png" alt="" />
+            <p>NEUTRAL</p>
+            <h1>
+              7 Things about <br /> web design your <br /> boss wants you to
+              <br /> know
+            </h1>
+          </div>
+          <div className="All__Blog__Posts">
+            <img src="./Frame--387.png" alt="" />
+            <p>NEUTRAL</p>
+            <h1>
+              7 Things about <br /> web design your <br /> boss wants you to
+              <br /> know
+            </h1>
+          </div>
+          <div className="All__Blog__Posts">
+            <img src="./Frame--387.png" alt="" />
+            <p>NEUTRAL</p>
+            <h1>
+              7 Things about <br /> web design your <br /> boss wants you to
+              <br /> know
+            </h1>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
