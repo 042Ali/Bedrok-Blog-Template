10c1cfacb39d3684a3c9a25f4af75ee07831c5e0
042Ali <ali20090609@gmail.com>
2025-02-23 21:51:54 +0400

    init

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,39 +1,54 @@
 import "./Footer.css";
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
 export default function Footer() {
+  const scrollToTop = () => {
+    window.scrollTo(0, 0);
+  };
+
   return (
     <>
       <footer className="footer">
         <h1>BEDROK</h1>
         <p>Copyright 2022</p>
 
-        <div class="footer-container">
-          <h4 class="footer-title">CATEGORIES</h4>
-          <ul class="footer-list">
-            <li class="footer-item">
-              {" "}
-              <NavLink to={"/Entrepreneurship"}>Entrepreneurship</NavLink>
+        <div className="footer-container">
+          <h4 className="footer-title">CATEGORIES</h4>
+          <ul className="footer-list">
+            <li className="footer-item">
+              <NavLink to="/Entrepreneurship" onClick={scrollToTop}>
+                Entrepreneurship
+              </NavLink>
             </li>
-            <li class="footer-item">Creator</li>
-            <li class="footer-item">Tech</li>
-            <li class="footer-item">All</li>
+            <li className="footer-item">Creator</li>
+            <li className="footer-item">Tech</li>
+            <li className="footer-item">All</li>
           </ul>
         </div>
-        <div class="footer-section">
-          <h4 class="section-title">PAGES</h4>
-          <ul class="section-list">
-            <li class="section-item">Home</li>
-            <li class="section-item">About</li>
-            <li class="section-item">Subscribe</li>
+
+        <div className="footer-section">
+          <h4 className="section-title">PAGES</h4>
+          <ul className="section-list">
+            <li className="section-item">
+              <NavLink to="/" onClick={scrollToTop}>
+                Home
+              </NavLink>
+            </li>
+            <li className="section-item">
+              <NavLink to="/about" onClick={scrollToTop}>
+                About
+              </NavLink>
+            </li>
+            <li className="section-item">Subscribe</li>
           </ul>
         </div>
-        <div class="contact-section">
-          <h4 class="contact-title">CONTACT</h4>
-          <ul class="contact-list">
-            <li class="contact-item">Twitter</li>
-            <li class="contact-item">LinkedIn</li>
-            <li style={{ display: "flex" }} class="contact-item">
+
+        <div className="contact-section">
+          <h4 className="contact-title">CONTACT</h4>
+          <ul className="contact-list">
+            <li className="contact-item">Twitter</li>
+            <li className="contact-item">LinkedIn</li>
+            <li style={{ display: "flex" }} className="contact-item">
               Email me
             </li>
           </ul>
